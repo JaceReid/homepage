55d99e4727eb3ab52567088809f31d41c90a38b7
JamesDevine42 <jamescdevine@gmail.com>
2021-09-07 17:51:31 +0200

    Updated index

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-var date = new Date();
-var hour = date.getHours();
-var min = date.getMinutes();
-document.write(hour.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}) + ":" + min.toLocaleString('en-US', {minimumIntegerDigits:2, useGrouping:false}));
-console.log("Done!");
+function formatAMPM(date) {
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var ampm = hours >= 12 ? 'pm' : 'am';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    var strTime = hours + ':' + minutes + ' ' + ampm;
+    return strTime;
+}
+
+console.log(formatAMPM(new Date));
+
+setInterval(function () {
+    var date = formatAMPM(new Date);
+    document.getElementById("clock").innerHTML = date.toLocaleString('en-US', { minimumIntegerDigits: 2, useGrouping: false })/* adding time to the div */
+    console.log("Done!");
+}, 1 * 1000);
